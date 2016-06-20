e2831f3b09f7e0f9d05940c5ff37aa6d4566f2a8
WinterR <ramizsabovic@gmail.com>
2016-06-20 22:00:28 +0200

    Add repo_sync.sh, for automatic downloading

---
+++ repo_sync.sh
@@ -0,0 +1,9 @@
+if ! [ -d .repo/local_manifests/ ]; then
+      curl --create-dirs -L -o .repo/local_manifests/msm7x27a_manifest.xml -O -L https://raw.github.com/FloppyTeam/local_manifest/cm-11.0/msm7x27a_manifest.xml
+fi
+
+if ! [ -d .repo/manifests/ ]; then
+      repo init -u git://github.com/CyanogenMod/android.git -b cm-11.0 -g all,-notdefault,-darwin
+fi
+
+repo sync -j16
