5764041a9539ff6f899e4a23e88187f94ac04c48
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-19 23:27:30 -0300

    wip: manifest: Initial CyanogenMod 12.1 BringUp!
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Generic Variables
-_android="4.4.4"
-_android_version="KitKat"
-_custom_android="cm-11.0"
-_custom_android_version="CyanogenMod11"
+_android="5.1.1"
+_android_version="LolliPop"
+_custom_android="cm-12.1"
+_custom_android_version="CyanogenMod12.1"
 _github_custom_android_place="CyanogenMod"
 _github_device_place="TeamHackLG"
 # Make loop for usage of 'break' to recursive exit
